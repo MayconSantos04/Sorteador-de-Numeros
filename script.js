94ec53ad9a2ea7ba218de927220fee5073cdfc54
MayconSantos04 <gadsence47@gmail.com>
2026-04-27 22:31:21 -0300

    New: adicão de codigo no project.

---
+++ script.js
@@ -0,0 +1,28 @@
+const buttonDraw = document.querySelector(".button-sortear");
+const resultInt = document.querySelector(".result");
+const resultValues = document.querySelector(".results-values");
+const spanResult = document.querySelector("#span-value");
+const resultValue = document.querySelector(".result-value");
+
+buttonDraw.addEventListener("click", () => {
+  const numberTotal = Number(document.querySelector("#num").value);
+  const numberMin = Number(document.querySelector("#min").value);
+  const numberMax = Number(document.querySelector("#max").value);
+
+  const creatResult = document.createElement("span");
+  creatResult.classList.add("animation-result");
+
+  const creatResultValue = document.createElement("div");
+  creatResultValue.classList.add("result-value");
+  creatResultValue.textContent = "";
+
+  const result =
+    Math.floor(Math.random() * (numberMax - numberMin + 1)) + numberMin;
+  console.log(result);
+
+  creatResultValue.textContent = result;
+
+  creatResult.append(creatResultValue);
+  resultValues.append(creatResult);
+  resultInt.append(resultValues);
+});
