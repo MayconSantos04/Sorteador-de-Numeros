9707b8653c4f28d5b8a2c940af9c3e67d2e59d9e
MayconSantos04 <gadsence47@gmail.com>
2026-05-03 18:43:59 -0300

    update logic programming and correction for bugs.

--- script.js
+++ script.js
@@ -1,30 +1,96 @@
 const buttonDraw = document.querySelector(".button-sortear");
 const resultInt = document.querySelector(".result");
 const resultValues = document.querySelector(".results-values");
-const spanResult = document.querySelector("#span-value");
-const resultValue = document.querySelector(".result-value");
+const buttonChk = document.querySelector(`input[type="checkbox"]`);
+const sectionSorteio = document.querySelector("#sorteio");
+const resultSorteio = document.querySelector("#result-sorteio");
+const sortearAgain = document.querySelector("#sortear-again");
+
+function reinicio() {
+  sortearAgain.classList.add("hidden");
+  resultSorteio.classList.add("hidden");
+
+  sectionSorteio.classList.remove("hidden");
+  resultValues.innerHTML = "";
+
+  document.querySelector("#num").value = "";
+  document.querySelector("#min").value = "";
+  document.querySelector("#max").value = "";
+
+  sectionSorteio.classList.add("animation");
+}
+
+sortearAgain.addEventListener("click", reinicio);
 
 buttonDraw.addEventListener("click", () => {
   const numberTotal = Number(document.querySelector("#num").value);
   const numberMin = Number(document.querySelector("#min").value);
   const numberMax = Number(document.querySelector("#max").value);
 
-  resultValues.innerHTML = "";
-
-  for (let i = 0; i < numberTotal; i++) {
-    setTimeout(() => {
-      const creatResult = document.createElement("span");
-      creatResult.classList.add("animation-result");
-
-      const creatResultValue = document.createElement("div");
-      creatResultValue.classList.add("result-value");
-
-      const result =
-        Math.floor(Math.random() * (numberMax - numberMin + 1)) + numberMin;
-      creatResultValue.textContent = result;
-
-      creatResult.append(creatResultValue);
-      resultValues.append(creatResult);
-    }, i * 5000);
+  if (numberTotal === 0 || numberMax === 0) {
+    alert("Digite algum valor nos campos.");
+    return;
+  } else if (numberMin > numberMax) {
+    alert("Valor mínimo não pode ser maior que o máximo.");
+    return;
   }
+
+  if (buttonChk.checked) {
+    const quantidadeDisponivel = numberMax - numberMin + 1;
+
+    if (numberTotal > quantidadeDisponivel) {
+      alert(
+        "A quantidade de números solicitada é maior que o intervalo disponível para sorteio sem repetições.",
+      );
+      return;
+    }
+  }
+
+  sectionSorteio.classList.add("hidden");
+  resultSorteio.classList.remove("hidden");
+
+  setTimeout(() => {
+    resultValues.innerHTML = "";
+
+    const numbersGenerated = [];
+
+    for (let i = 0; i < numberTotal; i++) {
+      setTimeout(() => {
+        const creatResult = document.createElement("span");
+        creatResult.classList.add("animation-result");
+
+        const creatResultValue = document.createElement("div");
+        creatResultValue.classList.add("result-value");
+
+        const creatResultBox = document.createElement("div");
+        creatResultBox.classList.add("result-box");
+
+        let result;
+
+        if (buttonChk.checked) {
+          do {
+            result =
+              Math.floor(Math.random() * (numberMax - numberMin + 1)) +
+              numberMin;
+          } while (numbersGenerated.includes(result));
+
+          numbersGenerated.push(result);
+        } else {
+          result =
+            Math.floor(Math.random() * (numberMax - numberMin + 1)) + numberMin;
+        }
+
+        creatResultValue.textContent = result;
+
+        creatResultBox.append(creatResult, creatResultValue);
+        resultValues.append(creatResultBox);
+
+        if (i === numberTotal - 1) {
+          setTimeout(() => {
+            sortearAgain.classList.remove("hidden");
+          }, 3000);
+        }
+      }, i * 4000);
+    }
+  }, 2000);
 });
